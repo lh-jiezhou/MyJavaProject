ce9a11bb9023311c95bcf081b2820616fe136a0b
lh-jiezhou <897587636@qq.com>
2021-09-06 19:31:12 +0800

    算法整理

--- algorithm-test/src/com/lh/Main.java
+++ algorithm-test/src/com/lh/Main.java
@@ -5,9 +5,72 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{1, 2, 3};
-        System.out.println(pivotIndex(nums));
+        int[][] nums = new int[][]{{1, 2,3},{4, 5,6},{7,8,9}};
+        int[] res =  findDiagonalOrder(nums);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
 
+    }
+
+    public static int[] findDiagonalOrder(int[][] mat) {
+        // 对角线 = m + n - 1
+        int m = mat.length;
+        int n = mat[0].length;
+        int index = m + n - 1;
+
+        List<Integer> res = new ArrayList<>();
+
+
+        for(int i=1; i<=index; i++){
+            List<Integer> temp = new ArrayList<>();
+
+            // 行起点 (0, i-1)
+            if(i-1<n){
+                for(int j=0, k=i-1; j<m && k>=0; j++, k--){
+                    temp.add(mat[j][k]);
+                }
+            }
+            // 列起点 (i-n, n-1)
+            else {
+                for(int j=i-n, k=n-1; j<m && k>=0; j++, k--){
+                    temp.add(mat[j][k]);
+                }
+            }
+            int[] t = new int[temp.size()];
+            for(int j=0; j<temp.size(); j++){
+                t[j] = temp.get(j);
+            }
+
+            // 奇数对角线(左下 -> 右上)
+            if(i%2 == 1){
+                reverse(t);
+            }
+            for(int k=0; k<t.length; k++){
+                res.add(t[k]);
+            }
+
+
+        }
+
+
+        System.out.println(res.size());
+        int[] result = new int[res.size()];
+        for(int i=0; i<res.size(); i++){
+            result[i] = res.get(i);
+        }
+        return result;
+
+
+
+    }
+
+    static void reverse(int[] nums){
+        for(int i=0; i<nums.length/2; i++){
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
     }
 
     public static int pivotIndex(int[] nums) {
