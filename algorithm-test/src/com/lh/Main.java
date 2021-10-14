eb8803fb8843fb7a969a79d108b703716d566213
lh-jiezhou <897587636@qq.com>
2021-10-14 09:18:30 +0800

    算法

--- algorithm-test/src/com/lh/Main.java
+++ algorithm-test/src/com/lh/Main.java
@@ -1,16 +1,35 @@
 package com.lh;
 import java.util.*;
 
-public class Main {
+public class Main implements Iterator<Integer> {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int k = sc.nextInt();
+    // 队列
+    Queue<Integer> queue;
 
+    public Main(Iterator<Integer> iterator) {
+        // initialize any member here.
+        queue = new LinkedList();
+        while (iterator.hasNext()){
+            queue.offer(iterator.next());
+        }
     }
 
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return queue.peek();
+    }
 
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        return queue.poll();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
 
 
 }
